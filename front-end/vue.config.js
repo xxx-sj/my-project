62ef5b37df3fd8ea0900d31c68f7e1a78b813970
loky13 <loky13@daum.net>
2023-06-15 23:15:35 +0900

    vue build setting
    build path 설정 및 개발 시 proxy 설정

--- front-end/vue.config.js
+++ front-end/vue.config.js
@@ -1,4 +1,29 @@
 const { defineConfig } = require('@vue/cli-service')
+
+const host = "localhost";
+const port = "8080";
+
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+
+  outputDir: "../back-end/src/main/resources/static",
+
+  devServer: {
+    hot: true,
+
+    proxy: {
+      //http://localhost:8081 -> http://localhost:8080
+      '/api/': {
+        // target: `http://${host}:${port}`,
+        target: `http://localhost:8080`,
+        changeOrigin: true,
+      },
+
+      '/ws/': {
+        target: `ws://${host}:${port}`,
+        changeOrigin: false,
+        ws: true,
+      }
+    }
+  }
 })
